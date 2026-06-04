fd93f7e5df8be96c51a17d74f6b1557ca06255ea
Lakshay-Hooda <idkwhy123490@gmail.com>
2026-06-04 23:56:33 +0530

    Add C++ program to print initials L H

---
+++ INITIALS.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+
+  std::cout << "L       H   H\n";
+  std::cout << "L       H   H\n";
+  std::cout << "L       H   H\n";
+  std::cout << "L       HHHHH\n";
+  std::cout << "L       H   H\n";
+  std::cout << "L       H   H\n";
+  std::cout << " LLLLLL H   H\n";
+}
